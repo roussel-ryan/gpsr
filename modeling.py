c15baef4675e54c35cefd16d1c4d18bd39295430
Ryan Roussel <rroussel@slac.stanford.edu>
2022-08-06 20:05:24 -0500

    add dropout to modeling

--- modeling.py
+++ modeling.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import torch
+from copy import deepcopy
+from torch.utils.data import Dataset
 
 from histogram import histogram2d
 from torch import nn
@@ -9,30 +11,127 @@ from torch_track import TorchQuad, TorchDrift, Beam
 
 
 class NonparametricTransform(torch.nn.Module):
-    def __init__(self, n_hidden, width):
+    def __init__(self, n_hidden, width, dropout=0.0, activation=torch.nn.Tanh()):
         """
         Nonparametric transformation - NN
         """
         super(NonparametricTransform, self).__init__()
 
-        layer_sequence = [torch.nn.Linear(6, width), torch.nn.Tanh()]
+        layer_sequence = [nn.Linear(6, width), activation]
 
-        for _ in range(n_hidden):
+        for i in range(n_hidden):
             layer_sequence.append(torch.nn.Linear(width, width))
-            layer_sequence.append(torch.nn.Tanh())
+            layer_sequence.append(torch.nn.Dropout(dropout))
+            layer_sequence.append(activation)
 
         layer_sequence.append(torch.nn.Linear(width, 6))
 
-        self.linear_tanh_stack = torch.nn.Sequential(
-            *layer_sequence
-        )
+        self.stack = torch.nn.Sequential(*layer_sequence)
 
     def forward(self, X):
         # scale inputs
-        #X = X * 1e3
-        X = self.linear_tanh_stack(X)
+        # X = X * 1e3
+        X = self.stack(X)
 
-        return X * 1e-3
+        return X * 1e-2
+
+class InitialBeam(torch.nn.Module):
+    def __init__(self, n, transformer, **kwargs):
+        super(InitialBeam, self).__init__()
+        self.transformer = transformer
+        self.n = n
+        self.kwargs = kwargs
+        # dist = torch.distributions.Uniform(-torch.ones(6), torch.ones(6))
+        dist = torch.distributions.MultivariateNormal(torch.zeros(6), torch.eye(6))
+        base_distribution_samples = dist.sample([n])
+
+        self.register_buffer("base_distribution_samples", base_distribution_samples)
+
+    def forward(self, X=None):
+        if X is None:
+            X = self.base_distribution_samples
+
+        tX = self.transformer(X)
+        return Beam(tX, **self.kwargs)
+
+
+# create data loader
+class ImageDataset(Dataset):
+    def __init__(self, k, images):
+        self.images = images
+        self.k = k
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        return self.k[idx], self.images[idx]
+
+
+class QuadScanModel(torch.nn.Module):
+    def __init__(self, initial_beam, transport, imager, condition=True,
+                 init_weights=None):
+        super(QuadScanModel, self).__init__()
+        self.beam_generator = deepcopy(initial_beam)
+
+        self.lattice = transport
+        self.imager = imager
+
+        if init_weights is not None:
+            self.beam_generator.apply(init_weights)
+
+        # condition initial beam
+        if condition:
+            self.beam_generator = condition_initial_beam(self.beam_generator)
+
+    def forward(self, K):
+        initial_beam = self.beam_generator()
+        output_beam = self.lattice(initial_beam, K)
+        output_coords = torch.cat(
+            [output_beam.x.unsqueeze(0), output_beam.y.unsqueeze(0)]
+        )
+        output_images = self.imager(output_coords)
+        # return output_images
+
+        #scalar_metric = 0
+        # calculate 6D emittance of input beam
+        # emit =
+        #cov = torch.cov(initial_beam.data.T)
+        scalar_metric = torch.norm(initial_beam.data, dim=1).pow(2).mean()
+        #scalar_metric = cov.trace()
+        return output_images, scalar_metric
+
+
+def beam_loss(out_beam, target_beam):
+    return torch.std(out_beam.data - target_beam * 1e-2)
+
+
+def condition_initial_beam(initial_beam):
+    optim = torch.optim.Adam(initial_beam.transformer.parameters(), lr=0.01)
+    n_iter = 2500
+    losses = []
+    test_beam = initial_beam.base_distribution_samples[:1000]
+
+    for i in trange(n_iter):
+        optim.zero_grad(set_to_none=True)
+
+        output_beam = initial_beam(test_beam)
+        loss = beam_loss(output_beam, test_beam)
+
+        losses += [loss.cpu().detach()]
+        loss.backward()
+
+        optim.step()
+
+    #plt.plot(losses)
+    #plt.show()
+
+    # check to make sure that the initial beam is a good fit to the base distribution
+    output_beam = initial_beam()
+    loss = beam_loss(output_beam, initial_beam.base_distribution_samples)
+    print(f"conditioning loss: {loss.cpu().detach()}")
+
+    return initial_beam
 
 
 class Imager(torch.nn.Module):
@@ -43,28 +142,6 @@ class Imager(torch.nn.Module):
 
     def forward(self, X):
         return histogram2d(X[0], X[1], self.bins, self.bandwidth)
-
-
-class InitialBeam(torch.nn.Module):
-    def __init__(self, n, n_hidden=2, width=100, **kwargs):
-        super(InitialBeam, self).__init__()
-        self.transformer = NonparametricTransform(n_hidden, width)
-        self.n = n
-        self.kwargs = kwargs
-        #dist = torch.distributions.Uniform(-torch.ones(6), torch.ones(6))
-        dist = torch.distributions.MultivariateNormal(torch.zeros(6), torch.eye(6))
-        base_distribution_samples = dist.sample([n])
-
-        self.register_buffer("base_distribution_samples", base_distribution_samples)
-
-
-    def forward(self, X=None):
-        if X is None:
-            X = self.base_distribution_samples
-
-        tX = self.transformer(X)
-        return Beam(tX, **self.kwargs)
-
 
 
 class InitialBeam2(torch.nn.Module):
