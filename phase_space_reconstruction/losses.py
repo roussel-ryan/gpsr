1bc19c7ecedcaa934bac82a7f392ff63c6ef39c6
Ryan Roussel <rroussel@uchicago.edu>
2023-01-25 14:23:00 -0800

    set defualt term weights to zero

--- phase_space_reconstruction/losses.py
+++ phase_space_reconstruction/losses.py
@@ -23,8 +23,8 @@ def mae_log_loss(target, pred):
 
 class MENTLoss(Module):
     def __init__(
-        self, lambda_, beta_=torch.tensor(1.0), gamma_=torch.tensor(1.0),
-            alpha_=torch.tensor(1.0), debug=False
+        self, lambda_, beta_=torch.tensor(0.0), gamma_=torch.tensor(0.0),
+            alpha_=torch.tensor(0.0), debug=False
     ):
         super(MENTLoss, self).__init__()
 
