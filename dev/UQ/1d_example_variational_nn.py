f7ec7e44dbf9adcadcbdcbdcbf69c0457f817198
Ryan Roussel <rroussel@slac.stanford.edu>
2023-05-19 10:25:35 -0500

    add UQ code to dev

--- dev/UQ/1d_example_variational_nn.py
+++ dev/UQ/1d_example_variational_nn.py
@@ -1,15 +1,13 @@
-from copy import deepcopy
-
 import matplotlib.pyplot as plt
 import torch
-from blitz.modules.weight_sampler import TrainableRandomDistribution
-from models import CoupledVariationalNN, precondition, IndependentVariationalNN
+
+from models import CoupledVariationalNN, precondition
 from phase_space_reconstruction.histogram import histogram
-from torch import nn
-from torch.nn.functional import kl_div, mse_loss
 
 
 def log_target_probability(x):
+    """returns the log of the target probability distribution"""
+
     bin_width = x[1] - x[0]
     out = (
             torch.distributions.Normal(torch.ones(1) * -0.5, 0.2 * torch.ones(1))
@@ -24,6 +22,11 @@ def log_target_probability(x):
 
 
 def log_pred_probability(x, y):
+    """
+    calculates a differentiable log-probability distribution at points `x` from
+    samples `y`
+
+    """
     bandwidth = torch.tensor(1e-1)
     bin_width = x[1] - x[0]
     hist = histogram(y, x, bandwidth).squeeze()
@@ -50,18 +53,21 @@ def log_predictive_likelihood(x, y, visualize=False):
         # ax2.plot(x,(log_target_p.exp()*(log_target_p - log_pred_p))[0].detach())
 
     # calc kl div between distributions
-    #return (log_target_p.exp() * (log_target_p - log_pred_p)).sum(dim=-1)
-    return torch.sum((log_target_p.exp() - log_pred_p.exp())**2, dim=-1)
+    return (
+            torch.sum(torch.abs(log_target_p - log_pred_p), dim=-1) /
+            log_target_p.shape[-1]
+    )
 
 
 test_x = torch.linspace(-1.5, 1.5, 100)
 n_part = 500
 
-n_samples = 10
+n_samples = 50
 model = CoupledVariationalNN()
 # base_X = torch.rand(n_part, 1).repeat(n_samples, 1, 1)*2.0 - 1.0
 
-base_X = torch.linspace(-1, 1, n_part).unsqueeze(-1).repeat(n_samples, 1, 1)
+# precondition model weights to predict a uniform distribution
+base_X = torch.linspace(-0.1, 0.1, n_part).unsqueeze(-1).repeat(n_samples, 1, 1)
 model = precondition(model, base_X)
 
 # show initial distribution
@@ -69,21 +75,22 @@ out = model(base_X).squeeze()
 fig, ax = plt.subplots()
 ax.hist(base_X[0].detach().numpy())
 fig, ax = plt.subplots()
-ax.hist(out[0].detach().numpy())
+for ele in out:
+    ax.hist(ele.detach().numpy(), histtype="step")
 
 log_predictive_likelihood(test_x, out, True)
 
 # train
-optim = torch.optim.Adam(model.parameters(), lr=0.001)
+optim = torch.optim.Adam(model.parameters(), lr=0.01)
 
 loss_track = []
-for i in range(3000):
+for i in range(2000):
     optim.zero_grad()
 
     # get samples
     out = model(base_X).squeeze()
     log_lk = log_predictive_likelihood(test_x, out, visualize=False).mean()
-    complexity = model.std(base_X).mean() * 1.0
+    complexity = model.std(base_X).mean() * 0.1
 
     # calculate loss
     loss = log_lk - complexity
@@ -92,13 +99,14 @@ for i in range(3000):
     loss_track += [torch.stack([loss, log_lk, complexity])]
 
     if i % 100 == 0:
-        print(log_lk, complexity, loss)
+        print(i, log_lk, complexity, loss)
 
     optim.step()
 
 loss_track = torch.stack(loss_track).detach()
 fig2, ax2 = plt.subplots()
-ax2.plot(loss_track)
+ax2.plot(loss_track, label=["total loss", "log lk", "complexity"])
+ax2.legend()
 
 # show final distribution
 fig, ax = plt.subplots()
@@ -106,11 +114,21 @@ out = model(base_X).squeeze()
 mean = model.mean(base_X).squeeze()[0].detach()
 std = model.std(base_X).squeeze()[0].detach()
 
-initial_dist = log_pred_probability(test_x, out).exp()
+initial_dist = log_pred_probability(test_x, out).exp().detach()
+initial_dist_mean = initial_dist.mean(dim=0)
+l = initial_dist.quantile(0.05, dim=0)
+u = initial_dist.quantile(0.95, dim=0)
+
+
 for i in range(base_X.shape[0]):
     ax.plot(test_x, initial_dist[i].detach())
 
-log_predictive_likelihood(test_x, out, True)
+log_target_p = log_target_probability(test_x)
+
+fig, ax = plt.subplots()
+ax.plot(test_x, log_target_p.detach().exp())
+ax.plot(test_x, initial_dist_mean)
+ax.fill_between(test_x, l, u, alpha=0.5)
 
 fig3, ax3 = plt.subplots()
 for i in range(len(mean))[::10]:
