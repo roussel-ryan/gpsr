bd842afbf61c564204c43f402548bff2857004f0
austin-hoover <austin.michael.hoover@gmail.com>
2025-06-13 16:47:54 -0400

    Fix GPU issue

--- docs/examples/example_reconstruction_4d_flow.py
+++ docs/examples/example_reconstruction_4d_flow.py
@@ -1,78 +1,62 @@
-"""Train a flow-based generative model in the GPSR framework.
+"""Train a flow-based generative model.
 
 We begin by training a regular NN generative mode on a set of measured images.
-The purpose of this step is two-fold. First, we want to check if a solution exists. 
-Entropy-based training may require multiple epochs with a more expensive generative 
-model, so this initial training should not add much to the overall training time. 
-Failure to fit the data with the NN likely points to an incorrecet forward model.
-(It is also possible (but unlikely) that the NN is not flexible enough to represent
-the phase space distribution.) 
+The purpose of this step is three-fold. First, we want to check if a solution 
+exists. Failure to fit the data likely points to an incorrect forward model.
+(It is also possible, but unlikely, that the NN cannot represent the true 
+distribution.) An entropy-regularized solution may require more iterations 
+with a more expensive generative model, so this initial training should not
+add much to the overall training time. Second, we want to estimate the 
+covariance matrix of the distribution. Flow-based generative models perform
+best when the scale or covariance of the distribution is known. For example,
+in density estimation, samples are typically scaled to identity covariance 
+before passing through the flow. The covariance matrix is also useful for 
+defining a prior distribution for relative entropy estimation. Third, we
+want to compare the NN solution to the entropy-regularized solution to see
+if there is any difference in features.
 
-Second, we want to estimate the covariance matrix of the distribution. The covariance
-matrix is typically overdetermined by the measurements, meaning all solutions should 
-have the same second-order moments (<xx>, <xy>, etc.) but possibly different higher-
-order moments. Flow-based generative models perform best when the scale or covariance
-of the distribution is known ahead of time. For example, in density estimation, samples
-are typically scaled to identity covariance before passing through the flow. The 
-covariance matrix is also useful for defining a prior distribution over the phase 
-space coordinates for relative entropy estimation.
-
-We then train a flow-based generative model to minimize the KL divergence between
-the reconstructed distribution :math:`p_{\theta}(x)` and the prior distribution 
-:math:`q(x)`, where :math:`\theta` represents the generative model parameters. The 
-KL divergence is the negative of the relative entropy, which we write as 
+After estimating the covariance matrix, we train a normalizing flow to 
+minimize the KL divergence between the reconstructed distribution 
+:math:`p_{\theta}(x)` and the prior distribution :math:`q(x)`, where 
+:math:`\theta` represents the generative model parameters. The KL 
+divergence is the negative of the relative entropy, which we write as 
 
 .. math:: 
     S(\theta) = S[p_{\theta}(x), q(x)] = \int p_{\theta}(x) ( \log p_{\theta}(x) - \log q(x) ) dx.
 
-The entropy can take values over the range :math:`[-\infty, 0]` with a maximum at
-:math:`S[p(x), q(x)] = 0` when p(x) = q(x).
+The entropy can take values over the range :math:`[-\infty, 0]` with a 
+maximum at :math:`S[p(x), q(x)] = 0` when p(x) = q(x).
 
-In this script, we train the model soft constraints and a penalty method. At each epoch,
-we minimize the loss
+In this script, we train the model soft constraints and a penalty method. At
+each epoch, we minimize the loss
 
 .. math:: L(theta) = -S(\theta) + \mu * D(\theta)
 
-with respect to :math:`\theta`, where :math:`D(\theta)` is the mean absolute error 
-between predicted and measured images and :math:`\mu` is a constant. The initial epoch 
-pulss the distribution to the prior, while subsequent epochs encourage consistency with
-the data.
+with respect to :math:`\theta`, where :math:`D(\theta)` is the mean absolute
+error between predicted and measured images and :math:`\mu` is a constant. The
+initial epoch pulls the distribution toward the prior, while subsequent epochs
+encourage consistency with the data.
 """
-
-# To do:
-# - Add callback to print/plot loss for data fit and entropy.
-# - Fix GPU issue with Pytorch Lighting.
-
-import abc
 import argparse
-import math
 import os
 import pathlib
-import sys
 import time
-from typing import Any
-from typing import Callable
 
-import matplotlib.pyplot as plt
+import torch
 import lightning
 import numpy as np
-import torch
-import zuko
-
-from cheetah.particles import ParticleBeam
-from cheetah.utils.bmadx import bmad_to_cheetah_coords
+import matplotlib.pyplot as plt
 
 from gpsr.beams import NNParticleBeamGenerator 
 from gpsr.beams import NSF
 from gpsr.beams import EntropyBeamGenerator
+from gpsr.beams import GaussianPrior
 from gpsr.modeling import GPSR
 from gpsr.modeling import GPSRLattice
 from gpsr.modeling import GPSRQuadScanLattice
 from gpsr.modeling import EntropyGPSR
 from gpsr.datasets import QuadScanDataset
 from gpsr.datasets import split_dataset
-from gpsr.losses import mae_loss
-from gpsr.losses import normalize_images
 from gpsr.train import LitGPSR
 from gpsr.train import EntropyLitGPSR
 
@@ -81,18 +65,30 @@ from gpsr.train import EntropyLitGPSR
 # --------------------------------------------------------------------------------------
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--nsamp", type=int, default=10_000)
+
+parser.add_argument("--flow-transforms", type=int, default=3)
+parser.add_argument("--flow-width", type=int, default=64)
+parser.add_argument("--flow-depth", type=int, default=3)
+parser.add_argument("--prior-scale", type=float, default=1.1)
+
+parser.add_argument("--nsamp", type=int, default=20_000)
 parser.add_argument("--iters-pre", type=int, default=250)
 parser.add_argument("--iters", type=int, default=250)
-parser.add_argument("--epochs", type=int, default=5)
+parser.add_argument("--epochs", type=int, default=8)
 parser.add_argument("--lr", type=float, default=0.001)
-parser.add_argument("--prior-scale", type=float, default=1.25)
 parser.add_argument("--penalty-min", type=float, default=1000.0)
 parser.add_argument("--penalty-max", type=float, default=None)
-parser.add_argument("--penalty-step", type=float, default=200.0)
+parser.add_argument("--penalty-step", type=float, default=1000.0)
 parser.add_argument("--penalty-scale", type=float, default=2.0)
-parser.add_argument("--eval-nsamp", type=int, default=100_000)
-parser.add_argument("--device", type=str, default="cpu")  # "auto", "cpu", "mps", ...
+
+parser.add_argument("--eval-nsamp", type=int, default=256_000)
+
+parser.add_argument("--plot-nsamp", type=int, default=256_000)
+parser.add_argument("--plot-bins", type=int, default=50)
+parser.add_argument("--plot-cmap", type=str, default="Blues")
+parser.add_argument("--plot-smooth", type=float, default=0.0)
+
+parser.add_argument("--device", type=str, default="cpu")
 parser.add_argument("--show", action="store_true")
 args = parser.parse_args()
 
@@ -130,55 +126,74 @@ gpsr_lattice = GPSRQuadScanLattice(l_quad=0.1, l_drift=1.0, screen=train_dset.sc
 # Covariance matrix estimation
 # --------------------------------------------------------------------------------------
 
-# Start with NN generator to estimate covariance matrix.
-gpsr_model = GPSR(NNParticleBeamGenerator(args.nsamp, p0c), gpsr_lattice)
+beam_generator = NNParticleBeamGenerator(args.nsamp, p0c)
+gpsr_model = GPSR(beam_generator, gpsr_lattice)
 train_loader = torch.utils.data.DataLoader(train_dset, batch_size=10)
 
 litgpsr = LitGPSR(gpsr_model)
 trainer = lightning.Trainer(accelerator=args.device, limit_train_batches=100, max_epochs=args.iters_pre)
 trainer.fit(model=litgpsr, train_dataloaders=train_loader)
 
-beam = litgpsr.gpsr_model.beam_generator()
+litgpsr.gpsr_model.eval()
+with torch.no_grad():
+    litgpsr.gpsr_model.beam_generator.set_base_particles(args.plot_nsamp)
+    beam = litgpsr.gpsr_model.beam_generator()
 
-# Analyze beam
-x = beam.particles[:, :6].detach().clone()
-cov_matrix = torch.cov(x.T)
-xmax = 4.0 * torch.std(x, axis=0)
-limits = [(-float(_xmax), float(_xmax)) for _xmax in xmax]
+    # Ignore longitudinal coordiantes
+    x = beam.particles[:, 0:6].detach().clone()
+    x[:, 4] = torch.randn(x.shape[0]) * 0.002
+    x[:, 5] = torch.randn(x.shape[0]) * 0.01
 
-fig, axs = beam.plot_distribution(
-    bins=50,
-    bin_ranges=limits,
-    plot_2d_kws=dict(
-        pcolormesh_kws=dict(cmap="Blues"),
-    ),
-)
-fig.set_size_inches(7.0, 7.0)
-if args.show:
-    plt.show()
-plt.savefig(os.path.join(output_dir, "fig_pre_corner.png"), dpi=300)
+    # Compute covariance matrix
+    cov_matrix = torch.cov(x.T)
+
+    # Compute plotting limits
+    xmax = 4.0 * torch.std(x, axis=0)
+    limits = [(-float(_xmax), float(_xmax)) for _xmax in xmax]
+
+    # Plot beam
+    fig, axs = beam.plot_distribution(
+        bins=args.plot_bins,
+        bin_ranges=limits,
+        plot_2d_kws=dict(
+            histogram_smoothing=args.plot_smooth,
+            pcolormesh_kws=dict(cmap=args.plot_cmap),
+        ),
+    )
+    fig.set_size_inches(9.0, 8.0)
+    fig.tight_layout()
+
+    if args.show:
+        plt.show()
+    plt.savefig(os.path.join(output_dir, "fig_pre_corner.png"), dpi=300)
+
+    # Make predictions
+    predictions = litgpsr.gpsr_model(train_dset.parameters)
+    pred_dset = QuadScanDataset(train_dset.parameters, predictions[0].detach(), train_dset.screen)
+
+    # Plot predictions
+    fig, ax = train_dset.plot_data(overlay_data=pred_dset)
+    fig.set_size_inches(18.0, 2.5)
+    fig.tight_layout()
+    if args.show:
+        plt.show()
+    plt.savefig(os.path.join(output_dir, f"fig_pre_images.png"), dpi=300)
 
 
-# We do not expect to reconstruct the 6D distribution from these measurements,
-# so we will get rid of the linear longitudinal-transverse correlations in
-# the covariance matrix.
-cov_matrix_old = torch.clone(cov_matrix)
-cov_matrix = torch.eye(ndim)
-cov_matrix[4, 4] = 1.0
-cov_matrix[5, 5] = 0.005
-cov_matrix[0:4, 0:4] = cov_matrix_old[0:4, 0:4]
-
-
-# Create flow-based model
+# Create normalizing flow
 # --------------------------------------------------------------------------------------
 
 gen_model = NSF(
-    ndim=ndim, cov_matrix=cov_matrix, transforms=3, hidden_units=64, hidden_layers=3
+    ndim=ndim, 
+    cov_matrix=cov_matrix, 
+    transforms=args.flow_transforms, 
+    hidden_units=args.flow_width, 
+    hidden_layers=args.flow_depth,
 )
 
 prior_loc = torch.zeros(ndim)
 prior_cov = cov_matrix * args.prior_scale**2
-prior = torch.distributions.MultivariateNormal(prior_loc, prior_cov)
+prior = GaussianPrior(prior_loc, prior_cov)
 
 beam_generator = EntropyBeamGenerator(
     gen_model=gen_model, 
@@ -197,7 +212,7 @@ train_loader = torch.utils.data.DataLoader(train_dset, batch_size=len(train_k_id
 
 litgpsr = EntropyLitGPSR(gpsr_model, lr=args.lr, penalty=args.penalty_min)
 trainer = lightning.Trainer(
-    accelerator=args.device,  # GPU not working
+    accelerator=args.device,
     limit_train_batches=100, 
     max_epochs=args.iters,
 )
@@ -217,37 +232,36 @@ for epoch in range(args.epochs):
 
     with torch.no_grad():
         # Increase particle count
-        gpsr_model.eval()
-        gpsr_model.beam_generator.n_particles = args.eval_nsamp
+        litgpsr.gpsr_model.eval()
+        litgpsr.gpsr_model.beam_generator.set_base_particles(args.plot_nsamp)
 
-        # Generate beam
-        beam, _ = gpsr_model.beam_generator()
-        print(beam.particles.shape)
-
-        # Make predictions
-        beam_out, entropy, predictions = gpsr_model(train_dset.parameters)
+        # Generate beam and predictions
+        beam, entropy, predictions = litgpsr.gpsr_model(train_dset.parameters)
         pred_dset = QuadScanDataset(train_dset.parameters, predictions[0].detach(), train_dset.screen)
-
-        # Plot predictions
-        fig, ax = train_dset.plot_data(overlay_data=pred_dset)
-        fig.set_size_inches(20, 3)
-        if args.show:
-            plt.show()
-        plt.savefig(os.path.join(output_dir, f"fig_images_{epoch:02.0f}.png"), dpi=300)
 
         # Plot beam (corner plot)
         fig, axs = beam.plot_distribution(
-            bins=50,
-            # bin_ranges=limits,
+            bins=args.plot_bins,
+            bin_ranges=limits,
             plot_2d_kws=dict(
-                pcolormesh_kws=dict(cmap="Blues"),
+                histogram_smoothing=args.plot_smooth,
+                pcolormesh_kws=dict(cmap=args.plot_cmap),
             ),
         )
-        fig.set_size_inches(7.0, 7.0)
+        fig.set_size_inches(9.0, 8.0)
+        fig.tight_layout()
         if args.show:
             plt.show()
         plt.savefig(os.path.join(output_dir, f"fig_corner_{epoch:02.0f}.png"), dpi=300)
 
+        # Plot predictions
+        fig, ax = train_dset.plot_data(overlay_data=pred_dset)
+        fig.set_size_inches(18.0, 2.5)
+        fig.tight_layout()
+        if args.show:
+            plt.show()
+        plt.savefig(os.path.join(output_dir, f"fig_images_{epoch:02.0f}.png"), dpi=300)
+
         # Reset particle count
-        gpsr_model.beam_generator.n_particles = args.nsamp
-        gpsr_model.train()
+        litgpsr.gpsr_model.beam_generator.set_base_particles(args.nsamp)
+        litgpsr.gpsr_model.train()
