d0be1214bf37865ab42a89e384ddb00e683833c3
jp-ga <97850904+jp-ga@users.noreply.github.com>
2026-07-01 00:07:34 -0700

    change eps to tiny

--- gpsr/losses.py
+++ gpsr/losses.py
@@ -21,7 +21,7 @@ def normalize_images(images):
     """
 
     sums = images.sum(dim=(-1, -2), keepdim=True)
-    return images / sums.clamp_min(torch.finfo(images.dtype).eps)
+    return images / sums.clamp_min(torch.finfo(images.dtype).tiny)
 
 
 def kl_div(target, pred):
